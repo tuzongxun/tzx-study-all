bbcfc552473faa995ef55f3b6eb9333017f98225
tuzongxun <1160569243@qq.com>
2020-10-16 15:01:34 +0800

    初始化提交

---
+++ src/main/java/com/tzx/study/demo/TzxStudyAllApplication.java
@@ -0,0 +1,13 @@
+package com.tzx.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TzxStudyAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TzxStudyAllApplication.class, args);
+	}
+
+}
